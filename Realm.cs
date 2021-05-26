fbad5f4324adbca02c9c587dda696001df8eb1d5
Insthync <ittipon.bay@gmail.com>
2021-05-26 14:37:05 +0700

    Change other service urls settings structure

--- Realm.cs
+++ Realm.cs
@@ -1,10 +1,12 @@
-﻿namespace Login
+﻿using System.Collections.Generic;
+
+namespace Login
 {
     public class Realm
     {
         public string Name { get; set; }
         public string ClusterUrl { get; set; }
         public string DatabaseUrl { get; set; }
-        public string[] OtherServiceUrls { get; set; }
+        public Dictionary<string, string> OtherServiceUrls { get; set; } = new Dictionary<string, string>();
     }
 }
